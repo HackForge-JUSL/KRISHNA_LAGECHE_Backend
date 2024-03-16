1ab13a7e4856ec2b0d06d30e049099ed83e6c3d5
Krishanu Saha <93394632+krishanu7@users.noreply.github.com>
2024-03-17 02:36:19 +0530

    Login Route

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,93 +1,108 @@
 import jwt from "jsonwebtoken";
-import bcryptjs from 'bcryptjs';
-import dotenv from 'dotenv';
+import bcryptjs from "bcryptjs";
+import dotenv from "dotenv";
 
 import Doctor from "../model/doctormodel.js";
-import Otpmodel from "../model/otpmodel.js"
+import Otpmodel from "../model/otpmodel.js";
 import Token from "../model/tokenmodel.js";
-
 
 dotenv.config();
 
-
 //SignUp Doctor
 export const signupDoctor = async (req, res) => {
-    try {
-        let doctor= await Doctor.findOne({email: req.body.email});
+  try {
+    let doctor = await Doctor.findOne({ email: req.body.email });
 
-        if(doctor){
-            return res.status(400).json({ msg: "Email Already Exists" });
-        }else{
-            const hashedPassword = await bcryptjs.hash(req.body.password, 10);
-            const doctor = { name: req.body.name, email: req.body.email, password: hashedPassword };
-            const newDoctor = new Doctor(doctor);
-            await newDoctor.save();
-            await Otpmodel.deleteMany({ email: req.body.email });
-            return res.status(200).json({ msg: "SignUp Successful" });
-        }
-    } catch (error) {
-        console.log("Error while Signing Up:", error);
-        return res.status(500).json({ msg: "Error in SignUp" });
+    if (doctor) {
+      return res.status(400).json({ msg: "Email Already Exists" });
+    } else {
+      const hashedPassword = await bcryptjs.hash(req.body.password, 10);
+      const doctor = {
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+      };
+      const newDoctor = new Doctor(doctor);
+      await newDoctor.save();
+      await Otpmodel.deleteMany({ email: req.body.email });
+      return res.status(200).json({ msg: "SignUp Successful" });
     }
-}
-
+  } catch (error) {
+    console.log("Error while Signing Up:", error);
+    return res.status(500).json({ msg: "Error in SignUp" });
+  }
+};
 
 //Login Doctor
-export const loginDoctor = async (req,res) => {
-    let doctor= await Doctor.findOne({email: req.body.email});
-    if(!doctor){
-        return res.status(400).json({ msg: "Username does not match" });
+export const loginDoctor = async (req, res) => {
+  let doctor = await Doctor.findOne({ email: req.body.email });
+  if (!doctor) {
+    return res.status(400).json({ msg: "Username does not match" });
+  }
+
+  try {
+    let match = await bcryptjs.compare(req.body.password, doctor.password);
+    if (match) {
+      const accessToken = jwt.sign(
+        doctor.toJSON(),
+        process.env.ACCESS_SECRET_KEY,
+        { expiresIn: "5d" }
+      );
+      const refreshToken = jwt.sign(
+        doctor.toJSON(),
+        process.env.REFRESH_SECRET_KEY
+      );
+
+      const newToken = new Token({ token: refreshToken });
+      await newToken.save();
+
+      return res
+        .status(200)
+        .json({
+          accessToken: accessToken,
+          refreshToken: refreshToken,
+          name: doctor.name,
+          email: doctor.email,
+        });
+    } else {
+      return res.status(400).json({ msg: "Password does not match" });
     }
-
-    try {
-        let match=await bcryptjs.compare(req.body.password,doctor.password);
-        if(match){
-            const accessToken=jwt.sign(doctor.toJSON(), process.env.ACCESS_SECRET_KEY, {expiresIn: '5d'});
-            const refreshToken=jwt.sign(doctor.toJSON(), process.env.REFRESH_SECRET_KEY);
-
-            const newToken=new Token({token: refreshToken});
-            await newToken.save();
-
-            return res.status(200).json({accessToken:accessToken,refreshToken:refreshToken,name:doctor.name,email:doctor.email});
-        }
-        else{
-            return res.status(400).json({ msg: "Password does not match" });
-        }
-    } catch (error) {
-        return res.status(500).json({ msg: "Error while login in doctor" });
-    }   
-}
+  } catch (error) {
+    return res.status(500).json({ msg: "Error while login in doctor" });
+  }
+};
 
 //Update Doctor
-export const updateDoctor = async (req,res) => {
-    try {
-        const data = req.body;
-        let doctor= await Doctor.findOne({email: req.user.email});
-        if(!doctor){
-            return res.status(400).json({ msg: "Username does not match" });
-        }
-        await Doctor.findOneAndUpdate({email: req.user.email}, {$set: data});
-        return res.status(200).json({ msg: "Details updated successfully" });
-    } catch (error) {
-        return res.status(500).json({ msg: "Error while updating details of doctor" });
-    }   
-}
-
-
-export const getAllDoctors = async (req,res) =>{
-    try {
-        const allDoctors = await Doctor.find({});
-        res.status(200).json(allDoctors);
-    } catch (error) {
-        return res.status(500).json({msg: "Error Getting All the Doctors"});
+export const updateDoctor = async (req, res) => {
+  try {
+    const data = req.body;
+    let doctor = await Doctor.findOne({ email: req.user.email });
+    if (!doctor) {
+      return res.status(400).json({ msg: "Username does not match" });
     }
-}
+    await Doctor.findOneAndUpdate({ email: req.user.email }, { $set: data });
+    return res.status(200).json({ msg: "Details updated successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ msg: "Error while updating details of doctor" });
+  }
+};
 
-export const getDoctor = async (req,res) =>{
-    try {
-        const doctor = await Post.findById(req.params.id);
-        res.status(200).json(doctor);
-    } catch (error) {
-        res.status(500).json({msg: "Error Getting Doctor"});
-    }
-}
+export const getAllDoctors = async (req, res) => {
+  try {
+    const allDoctors = await Doctor.find({}).select("-password");
+    res.status(200).json(allDoctors);
+  } catch (error) {
+    return res.status(500).json({ msg: "Error Getting All the Doctors" });
+  }
+};
+
+export const getDoctor = async (req, res) => {
+  try {
+    const doctor = await Post.findById(req.params.id);
+    res.status(200).json(doctor);
+  } catch (error) {
+    res.status(500).json({ msg: "Error Getting Doctor" });
+  }
+};
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,74 +1,90 @@
 import jwt from "jsonwebtoken";
-import bcryptjs from 'bcryptjs';
-import dotenv from 'dotenv';
+import bcryptjs from "bcryptjs";
+import dotenv from "dotenv";
 
 import User from "../model/usermodel.js";
-import Otpmodel from "../model/otpmodel.js"
+import Otpmodel from "../model/otpmodel.js";
 import Token from "../model/tokenmodel.js";
-
 
 dotenv.config();
 
-
 //SignUp User
 export const signupUser = async (req, res) => {
-    try {
-        let user= await User.findOne({email: req.body.email});
+  try {
+    let user = await User.findOne({ email: req.body.email });
 
-        if(user){
-            return res.status(400).json({ msg: "Email Already Exists" });
-        }else{
-            const hashedPassword = await bcryptjs.hash(req.body.password, 10);
-            const user = { name: req.body.name, email: req.body.email, password: hashedPassword };
-            const newUser = new User(user);
-            await newUser.save();
-            await Otpmodel.deleteMany({ email: req.body.email });
-            return res.status(200).json({ msg: "SignUp Successful" });
-        }
-    } catch (error) {
-        console.log("Error while Signing Up:", error);
-        return res.status(500).json({ msg: "Error in SignUp" });
+    if (user) {
+      return res.status(400).json({ msg: "Email Already Exists" });
+    } else {
+      const hashedPassword = await bcryptjs.hash(req.body.password, 10);
+      const user = {
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+      };
+      const newUser = new User(user);
+      await newUser.save();
+      await Otpmodel.deleteMany({ email: req.body.email });
+      return res.status(200).json({ msg: "SignUp Successful" });
     }
-}
-
+  } catch (error) {
+    console.log("Error while Signing Up:", error);
+    return res.status(500).json({ msg: "Error in SignUp" });
+  }
+};
 
 //Login User
-export const loginUser = async (req,res) => {
-    let user= await User.findOne({email: req.body.email});
-    if(!user){
-        return res.status(400).json({ msg: "Username does not match" });
+export const loginUser = async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return res.status(400).json({ msg: "Username does not match" });
+  }
+
+  try {
+    let match = await bcryptjs.compare(req.body.password, user.password);
+    if (match) {
+      const accessToken = jwt.sign(
+        user.toJSON(),
+        process.env.ACCESS_SECRET_KEY,
+        { expiresIn: "5d" }
+      );
+      const refreshToken = jwt.sign(
+        user.toJSON(),
+        process.env.REFRESH_SECRET_KEY
+      );
+
+      const newToken = new Token({ token: refreshToken });
+      await newToken.save();
+
+      const { password, ...rest } = user._doc;
+      return res.status(200).json({
+        status: true,
+        msg: "Successfully login",
+        accessToken,
+        refreshToken,
+        data: { ...rest },
+      });
+    } else {
+      return res.status(400).json({ msg: "Password does not match" });
     }
-
-    try {
-        let match=await bcryptjs.compare(req.body.password,user.password);
-        if(match){
-            const accessToken=jwt.sign(user.toJSON(), process.env.ACCESS_SECRET_KEY, {expiresIn: '5d'});
-            const refreshToken=jwt.sign(user.toJSON(), process.env.REFRESH_SECRET_KEY);
-
-            const newToken=new Token({token: refreshToken});
-            await newToken.save();
-
-            return res.status(200).json({accessToken:accessToken,refreshToken:refreshToken,name:user.name,email:user.email});
-        }
-        else{
-            return res.status(400).json({ msg: "Password does not match" });
-        }
-    } catch (error) {
-        return res.status(500).json({ msg: "Error while login in user" });
-    }   
-}
+  } catch (error) {
+    return res.status(500).json({ msg: "Error while login in user" });
+  }
+};
 
 //Update User
-export const updateUser = async (req,res) => {
-    try {
-        const data = req.body;
-        let user= await User.findOne({email: req.user.email});
-        if(!user){
-            return res.status(400).json({ msg: "Username does not match" });
-        }
-        await User.findOneAndUpdate({email: req.user.email}, {$set: data});
-        return res.status(200).json({ msg: "Details updated successfully" });
-    } catch (error) {
-        return res.status(500).json({ msg: "Error while updating details of user" });
-    } 
-}
+export const updateUser = async (req, res) => {
+  try {
+    const data = req.body;
+    let user = await User.findOne({ email: req.user.email });
+    if (!user) {
+      return res.status(400).json({ msg: "Username does not match" });
+    }
+    await User.findOneAndUpdate({ email: req.user.email }, { $set: data });
+    return res.status(200).json({ msg: "Details updated successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ msg: "Error while updating details of user" });
+  }
+};
