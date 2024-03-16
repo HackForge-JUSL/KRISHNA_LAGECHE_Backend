80829c63990ef6ad2760fa39968087a47871bd2e
Sumit Agarwal <agawralsumit334ju@gmail.com>
2024-03-16 13:06:38 +0530

    first commit

---
+++ database/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+const Connection= async()=>{
+    try {
+        const connectionInstance=await mongoose.connect(process.env.MONGODB_URI);
+        console.log("DataBase Connection Successfull:",connectionInstance.connection.host);
+    } catch (error) {
+        console.log("Error connecting the database ",error);
+    }
+}
+
+export default Connection;
---
+++ index.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+    
+import Connection from './database/db.js';
+import router from './routes/route.js';
+dotenv.config();
+
+const PORT= process.env.PORT || 8000;
+const app=express();
+
+app.use(cors());
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/',router);
+
+
+app.listen(PORT,()=> console.log(`Server is running successfully on PORT ${PORT}`));
+
+Connection();
